eddb2321e3c6d87e81d2cd03e7b729de82d283a7
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-07-23 20:20:37 +0500

    Add files via upload

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -8,6 +8,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.addons.transition.FlxTransitionableState;
 import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
@@ -155,10 +156,10 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		changeSelection();
 		reloadCheckboxes();
 
-		#if android
-		addVirtualPad(LEFT_FULL, A_B_C);
-		addPadCamera();
-		#end
+                #if android
+                addVirtualPad(LEFT_FULL, A_B_C);
+                addVirtualPadCamera();
+                #end
 	}
 
 	var nextAccept:Int = 5;
@@ -177,11 +178,11 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		if (controls.BACK) {
 			#if android
-			FlxTransitionableState.skipNextTransOut = true;
+         FlxTransitionableState.skipNextTransOut = true;
 			FlxG.resetState();
-			#else
-			close();
-			#end
+         #else
+         close();
+         #end
 			ClientPrefs.saveSettings();
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
