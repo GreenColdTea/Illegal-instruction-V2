4f8a4af23a232dda2d461dbc6c618594c9c41749
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-07-23 20:58:50 +0500

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1727,7 +1727,7 @@ class PlayState extends MusicBeatState
 			blah.destroy();
 			luaDebugGroup.remove(blah);
 		}
-		luaDebugGroup.insert(0, new DebugLuaText(text, luaDebugGroup, FlxColor.WHITE));
+		luaDebugGroup.insert(0, new DebugLuaText(text, FlxColor.WHITE));
 		#end
 	}
 
