27cbadb7910996db904762f2d33f20d99e9c6100
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-07-24 15:11:01 +0500

    Update Shader.hx

--- source/openfl/display/Shader.hx
+++ source/openfl/display/Shader.hx
@@ -22,7 +22,7 @@ import openfl.display.ShaderParameterType;
  * @see https://github.com/openfl/openfl/blob/develop/src/openfl/utils/_internal/ShaderMacro.hx
  * @see https://dixonary.co.uk/blog/shadertoy
  */
-class FlxRuntimeShader extends FlxShader
+class Shader extends FlxShader
 {
 	#if FLX_DRAW_QUADS
 	// We need to add stuff from FlxGraphicsShader too!
