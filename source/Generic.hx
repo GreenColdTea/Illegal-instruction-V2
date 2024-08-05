94e7763e402b9de83fbfb66ff81884bf92ae2cf4
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-08-05 20:34:38 +0500

    Update Generic.hx

--- source/Generic.hx
+++ source/Generic.hx
@@ -125,7 +125,7 @@ class Generic {
 			trace('Couldn\'t save error message. (${e.message})');
 		#end
 
-		SUtil.showPopUp('$m\n$stackLabel', "Error!");
+		showPopUp('$m\n$stackLabel', "Error!");
 
 		#if html5
 		if (FlxG.sound.music != null)
@@ -135,7 +135,7 @@ class Generic {
 		#else
 		System.exit(1);
 		#end
-	        }
+	        });
 	    }
 		
 	
