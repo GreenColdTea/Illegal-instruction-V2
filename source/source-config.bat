1d2636bd551e428eca0125036fdf8a0d13eeabef
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-08-16 10:51:15 +0500

    Update source-config.bat

--- source/source-config.bat
+++ source/source-config.bat
@@ -1,15 +1,22 @@
 @echo off
 cls
-title Illegal Instruction Auto-Downloader
+title Illegal Instructions Necessary Libraries for Compiling
 echo.
-echo Installing echo...
+echo Installing flixel 5.0.0...
 echo.
-haxelib install echo
+haxelib install flixel 5.0.0 --quiet
 echo.
-echo Installing echo-flixel...
+echo Installing flixel-addons 2.12.0...
 echo.
-haxelib install echo-flixel
+haxelib install flixel-addons 2.12.0 --quiet
 echo.
+echo Installing openfl 9.2.0...
+echo.
+haxelib install openfl 9.3.0 --quiet
+echo.
+echo Installing lime 8.0.1
+echo.
+haxelib install lime 8.0.1 --quiet
 echo.
 echo Downloaded! Press any key to close the app!
 pause
