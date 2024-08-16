98a32ffba4ea9deab20503c801a0a987e2eacf26
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-08-17 01:40:27 +0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1073,7 +1073,7 @@ class PlayState extends MusicBeatState
 		#elseif mobile
 		for (folder in foldersToCheck) {
                     if (OpenFlAssets.exists(folder)) {
-                    var files = lime.system.System.getDirectory(folder);
+                    var files = FileSystem.readDirectory(folder);
                     for (file in files) {
                        if (file.endsWith('.lua') && !filesPushed.contains(file)) {
                           luaArray.push(new FunkinLua(folder + '/' + file));
