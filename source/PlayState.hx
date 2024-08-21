e09db79411a189f36c959cb90a969bd5f9864d5f
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-08-21 21:28:04 +0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1258,7 +1258,6 @@ class PlayState extends MusicBeatState
 		timeBar = new FlxBar(timeBarBG.x + 4, timeBarBG.y + 4, LEFT_TO_RIGHT, Std.int(timeBarBG.width - 8), Std.int(timeBarBG.height - 8), this,
 			'songPercent', 0, 1);
 		timeBar.scrollFactor.set();
-		timeBar.createFilledBar(0xFF000000, dadColor);
 		timeBar.numDivisions = 800; //How much lag this causes?? Should i tone it down to idk, 400 or 200?
 		timeBar.alpha = 0;
 		timeBar.visible = showTime;
@@ -1267,7 +1266,6 @@ class PlayState extends MusicBeatState
 		fakeTimeBar = new FlxBar(timeBarBG.x + 4, timeBarBG.y + 4, LEFT_TO_RIGHT, Std.int(timeBarBG.width - 8), Std.int(timeBarBG.height - 8), this,
 			'fakeSongPercent', 0, 100);
 		fakeTimeBar.scrollFactor.set();
-		fakeTimeBar.createFilledBar(0xFF000000, dadColor);
 		fakeTimeBar.numDivisions = 1000;
 		fakeTimeBar.visible = showTime;
 
@@ -2259,6 +2257,9 @@ class PlayState extends MusicBeatState
         var dadColB = dad.healthColorArray[2];
 
 	var dadColor = (0xFF << 24) | (dadColR << 16) | (dadColG << 8) | dadColB;
+
+	fakeTimeBar.createFilledBar(0xFF000000, dadColor);
+	timeBar.createFilledBar(0xFF000000, dadColor);
 
 	var debugNum:Int = 0;
 	private var noteTypeMap:Map<String, Bool> = new Map<String, Bool>();
