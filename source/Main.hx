6471eb4e562ef1b56f8605218dfe2df46ced3cb2
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-09-03 01:38:12 +0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -122,6 +122,28 @@ class Main extends Sprite
         if(fpsVar != null) {
             fpsVar.visible = ClientPrefs.showFPS;
         }
+
+	FlxG.signals.gameResized.add(function (w, h) {
+	    if(fpsVar != null)
+		fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
+	    if (FlxG.cameras != null) {
+		for (cam in FlxG.cameras.list) {
+		     if (cam != null && cam.filters != null)
+			 resetSpriteCache(cam.flashSprite);
+		}
+	    }
+
+	    if (FlxG.game != null)
+		resetSpriteCache(FlxG.game);
+	});
+
+	#if mobile
+	FlxG.scaleMode = new MobileScaleMode();
+	#end
+
+	#if android 
+	FlxG.android.preventDefaultKeys = [BACK]; 
+	#end
     }
 
     #if (cpp || hl)
