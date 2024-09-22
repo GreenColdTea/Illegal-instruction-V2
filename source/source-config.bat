a393af4fa9c329bd9e0d5d3cbdc9e49ab2426863
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-09-22 10:27:06 +0500

    Update source-config.bat

--- source/source-config.bat
+++ source/source-config.bat
@@ -2,21 +2,21 @@
 cls
 title Illegal Instructions Necessary Libraries Installer for Compiling
 echo.
-echo Installing flixel 5.0.0...
+echo Setting flixel 5.0.0...
 echo.
-haxelib install flixel 5.0.0 --quiet
+haxelib set flixel 5.0.0 --quiet
 echo.
-echo Installing flixel-addons 2.12.0...
+echo Setting flixel-addons 2.12.0...
 echo.
-haxelib install flixel-addons 2.12.0 --quiet
+haxelib set flixel-addons 2.12.0 --quiet
 echo.
-echo Installing openfl 9.2.0...
+echo Installing openfl 9.3.0...
 echo.
-haxelib install openfl 9.3.0 --quiet
+haxelib Setting openfl 9.3.0 --quiet
 echo.
-echo Installing lime 8.0.1...
+echo Setting lime 8.0.1...
 echo.
-haxelib install lime 8.0.1 --quiet
+haxelib set lime 8.0.1 --quiet
 echo.
-echo Downloaded! Press any key to close the app!
+echo Done! Press any key to close the app!
 pause
