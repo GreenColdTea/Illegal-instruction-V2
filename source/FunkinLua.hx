e3ba3f5618de3f46b3dcc7ec24187a27384eff26
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-09-22 10:07:37 +0500

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -195,6 +195,7 @@ class FunkinLua {
 		Lua_helper.add_callback(lua, "addLuaScript", function(luaFile:String, ?ignoreAlreadyRunning:Bool = false) { //would be dope asf. 
 			var cervix = luaFile + ".lua";
 			var doPush = false;
+			#if MODS_ALLOWED
 			if(FileSystem.exists(Paths.modFolders(cervix))) {
 				cervix = Paths.modFolders(cervix);
 				doPush = true;
@@ -203,6 +204,11 @@ class FunkinLua {
 				if(FileSystem.exists(cervix)) {
 					doPush = true;
 				}
+			}
+			#else
+			cervix = Paths.getPreloadPath(cervix);
+			if(FileSystem.exists(cervix)) {
+				doPush = true;
 			}
 
 			if(doPush)
@@ -226,6 +232,7 @@ class FunkinLua {
 		Lua_helper.add_callback(lua, "removeLuaScript", function(luaFile:String, ?ignoreAlreadyRunning:Bool = false) { //would be dope asf. 
 			var cervix = luaFile + ".lua";
 			var doPush = false;
+			#if MODS_ALLOWED
 			if(FileSystem.exists(Paths.modFolders(cervix))) {
 				cervix = Paths.modFolders(cervix);
 				doPush = true;
@@ -235,6 +242,12 @@ class FunkinLua {
 					doPush = true;
 				}
 			}
+			#else
+			cervix = Paths.getPreloadPath(cervix);
+			if(FileSystem.exists(cervix)) {
+				doPush = true;
+			}
+			#end
 
 			if(doPush)
 			{
