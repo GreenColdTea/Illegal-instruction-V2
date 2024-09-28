6edd647cb473b3c449463b62bc7945adeb0b3596
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-09-28 08:00:54 +0500

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -95,10 +95,10 @@ class PauseSubState extends MusicBeatSubstate
 		pauseArt.alpha = 0;
 	        if (PlayState.SONG.player2 == 'duke') {
 		pauseArt.x = renderDistance * -1 + 100;
-	        pauseArt.y -= 575;
+	        pauseArt.y += 50;
 		}
 	        else if (PlayState.SONG.player2 == 'chaotix' || PlayState.SONG.player2 == 'chaotix-rimlit') {
-	        pauseArt.y -= 400;
+	        pauseArt.y += 100;
 		}
 
 		var levelInfo:FlxText = new FlxText(20, 15, 0, "", 32);
