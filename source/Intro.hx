394c4456f445d809041944c10d2cc23d21ff5f82
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2024-09-29 19:16:05 +0500

    Update Intro.hx

--- source/Intro.hx
+++ source/Intro.hx
@@ -1,10 +1,30 @@
 package;
 
+import flixel.graphics.FlxGraphic;
+import sys.FileSystem;
 import flixel.FlxG;
+import flixel.FlxState;
 import flixel.input.keyboard.FlxKey;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+//import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
 import flixel.system.FlxSound;
+import flixel.system.ui.FlxSoundTray;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+import openfl.Assets;
 import hxcodec.VideoHandler;
-
 #if desktop
 import Discord.DiscordClient;
 import sys.thread.Thread;
