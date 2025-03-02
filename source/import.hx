0a90a3770c5447663bfd0c07051de03a6bc9bd3b
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-03 02:46:49 +0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -19,6 +19,24 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.addons.transition.FlxTransitionableState;
 import shaders.flixel.FlxShader;
 
+// Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+#if sys
+import sys.*;
+import sys.io.*;
+#end
+
 using StringTools;
 #end
 
