3a0a0bbee9d3261402072c0ac6cd4f5f96378681
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-03 06:52:43 +0500

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -361,4 +361,11 @@ class MusicBeatState extends FlxUIState
 	{
 		//trace('Section: ' + curSection + ', Beat: ' + curBeat + ', Step: ' + curStep);
 	}
+
+	function getBeatsOnSection()
+	{
+		var val:Null<Float> = 4;
+		if(PlayState.SONG != null && PlayState.SONG.notes[curSection] != null) val = PlayState.SONG.notes[curSection].sectionBeats;
+		return val == null ? 4 : val;
+	}
 }
