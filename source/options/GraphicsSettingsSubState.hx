a4fa39cd6168236c0f0ce3789afb4043fab96076
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-04 03:06:36 +0500

    Update GraphicsSettingsSubState.hx

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -93,7 +93,7 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		addOption(option);
 
 		#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
-		var option:Option = new Option('Framerate',
+		var option:Option = new Option('Framerate:',
 			"Pretty self explanatory, isn't it?",
 			'framerate',
 			'int',
