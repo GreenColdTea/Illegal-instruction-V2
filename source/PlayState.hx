c6b806276b9cdafd6fdc4d95e94e484d32f27d38
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-03 21:37:33 +0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2710,11 +2710,7 @@ class PlayState extends MusicBeatState
 		// SONG SPECIFIC SCRIPTS
 		#if LUA_ALLOWED
 		var filesPushed:Array<String> = [];
-	        #if !android
-		var foldersToCheck:Array<String> = [Paths.getPreloadPath('data/' + Paths.formatToSongPath(SONG.song) + '/')];
-	        #else
-		var foldersToCheck:Array<String> = [Generic.returnPath() + 'data/' + Paths.formatToSongPath(SONG.song) + '/'];
-	        #end
+		var foldersToCheck:Array<String> = [Paths.getPreloadPath('data/' + Paths.formatToSongPath(SONG.song) + '/'), Paths.getPath() + 'data/' + Paths.formatToSongPath(SONG.song) + '/'];
 
 		#if MODS_ALLOWED
 		foldersToCheck.insert(0, Paths.mods('data/' + Paths.formatToSongPath(SONG.song) + '/'));
