b3573f3760dc26f28f282deaefe7d0bfe03aac14
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-04 20:58:02 +0500

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -279,29 +279,21 @@ class Paths
 		return file;
 	}
 
-	inline static public function voices(song:String):Dynamic
-        {
-                var songKey:String = '${song.toLowerCase().replace(" ", "-")}/voices';
-                var voicesPath:String = getPath('songs/$songKey.$SOUND_EXT', SOUND);
+	inline static public function voices(song:String, postfix:String = null):Any
+	{
+		var songKey:String = '${formatToSongPath(song)}/voices';
+		if(postfix != null) songKey += '-' + postfix;
+		//trace('songKey test: $songKey');
+		var voices = returnSound(null, songKey, 'songs');
+		return voices;
+	}
 
-                if (!OpenFlAssets.exists(voicesPath)) {
-                    songKey = '${song.toLowerCase().replace(" ", "-")}/Voices';
-                    voicesPath = getPath('songs/$songKey.$SOUND_EXT', SOUND);
-                }
-                return returnSound('songs', songKey);
-        }
-		
-	inline static public function inst(song:String):Dynamic
-        {
-                var songKey:String = '${song.toLowerCase().replace(" ", "-")}/inst';
-                var instPath:String = getPath('songs/$songKey.$SOUND_EXT', SOUND);
-
-                if (!OpenFlAssets.exists(instPath)) {
-                    songKey = '${song.toLowerCase().replace(" ", "-")}/Inst';
-                    instPath = getPath('songs/$songKey.$SOUND_EXT', SOUND);
-                }
-                return returnSound('songs', songKey);
-        }	
+	inline static public function inst(song:String):Any
+	{
+		var songKey:String = '${formatToSongPath(song)}/inst';
+		var inst = returnSound(null, songKey, 'songs');
+		return inst;
+	}
 
 	inline static public function image(key:String, ?library:String, ?allowGPU:Bool = false):Dynamic
 	{
@@ -458,26 +450,40 @@ class Paths
 		return null;
 	}
 	
-	
-	public static function returnSound(path:String, key:String, ?library:String) {
+	public static function returnSound(path:Null<String>, key:String, ?library:String) {
 		#if MODS_ALLOWED
-		var file:String = modsSounds(path, key);
+		var modLibPath:String = '';
+		if (library != null) modLibPath = '$library/';
+		if (path != null) modLibPath += '$path';
+
+		var file:String = modsSounds(modLibPath, key);
 		if(FileSystem.exists(file)) {
-			if(!currentTrackedSounds.exists(file)) {
+			if(!currentTrackedSounds.exists(file))
+			{
 				currentTrackedSounds.set(file, Sound.fromFile(file));
+				//trace('precached mod sound: $file');
 			}
-			localTrackedAssets.push(key);
+			localTrackedAssets.push(file);
 			return currentTrackedSounds.get(file);
 		}
 		#end
+
 		// I hate this so god damn much
-		var gottenPath:String = getPath('$path/$key.$SOUND_EXT', SOUND, library);	
+		var gottenPath:String = '$key.$SOUND_EXT';
+		if(path != null) gottenPath = '$path/$gottenPath';
+		gottenPath = getPath(gottenPath, SOUND, library);
 		gottenPath = gottenPath.substring(gottenPath.indexOf(':') + 1, gottenPath.length);
 		// trace(gottenPath);
 		if(!currentTrackedSounds.exists(gottenPath))
-				  
-			currentTrackedSounds.set(gottenPath, Sound.fromFile(gottenPath));
-	
+		{
+			var retKey:String = (path != null) ? '$path/$key' : key;
+			retKey = ((path == 'songs') ? 'songs:' : '') + getPath('$retKey.$SOUND_EXT', SOUND, library);
+			if(OpenFlAssets.exists(retKey, SOUND))
+			{
+				currentTrackedSounds.set(gottenPath, OpenFlAssets.getSound(retKey));
+				//trace('precached vanilla sound: $retKey');
+			}
+		}
 		localTrackedAssets.push(gottenPath);
 		return currentTrackedSounds.get(gottenPath);
 	}
