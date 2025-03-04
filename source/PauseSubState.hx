e32dbb76f388bb5fe86db4768e78a3883392e9c4
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-04 22:40:53 +0500

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -453,38 +453,41 @@ class PauseSubState extends MusicBeatSubstate
 	}
 
 	function regenMenu():Void {
-		for (i in 0...grpMenuShit.members.length) {
-			var obj = grpMenuShit.members[0];
-			obj.kill();
-			grpMenuShit.remove(obj, true);
-			obj.destroy();
-		}
+                for (i in 0...grpMenuShit.members.length) {
+                        var obj = grpMenuShit.members[0];
+                        obj.kill();
+                        grpMenuShit.remove(obj, true);
+                        obj.destroy();
+                }
 
-		for (i in 0...menuItems.length) {
-			var item = new Alphabet(0, 70, menuItems[i], true, false);
-			item.isMenuItem = true;
-			if (PlayState.SONG.song.toLowerCase() == "found-you-legacy") {
-				item.textFont = "sonic-cd-menu-font.ttf";
-			}
-			item.targetY = i;
-			grpMenuShit.add(item);
+                var itemHeight = 70;
+                var totalHeight = menuItems.length * itemHeight;
+                var startY = (FlxG.height / 2) - (totalHeight / 2);
 
-			if(menuItems[i] == 'Skip Time:')
-			{
-				skipTimeText = new FlxText(0, 0, 0, '', 41);
-				skipTimeText.setFormat(Paths.font("chaotix.ttf"), 41, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-				skipTimeText.scrollFactor.set();
-				skipTimeText.y += 10;
-				skipTimeText.borderSize = 2;
-				skipTimeTracker = item;
-				add(skipTimeText);
+                for (i in 0...menuItems.length) {
+                        var item = new FlxText(0, startY + (i * itemHeight), 0, menuItems[i], 32);
+                        item.setFormat(Paths.font("sonic-cd-menu-font.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+                        item.borderSize = 2;
+                        item.scrollFactor.set();
+                        item.screenCenter(X);
+	                item.x -= 70;
+                        grpMenuShit.add(item);
 
-				updateSkipTextStuff();
-				updateSkipTimeText();
-			}
-		}
-		curSelected = 0;
-		changeSelection();
+                        if (menuItems[i] == 'Skip Time:') {
+                                skipTimeText = new FlxText(0, item.y + 10, 0, '', 41);
+                                skipTimeText.setFormat(Paths.font("chaotix.ttf"), 41, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+                                skipTimeText.borderSize = 2;
+                                skipTimeText.scrollFactor.set();
+                                skipTimeTracker = item;
+                                add(skipTimeText);
+
+                                updateSkipTextStuff();
+                                updateSkipTimeText();
+                        }
+                  }
+
+                  curSelected = 0;
+                  changeSelection();
 	}
 	
 	function updateSkipTextStuff()
