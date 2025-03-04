c690a934c6b517e269e32509af1dfe5f21d889f0
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-04 20:22:00 +0500

    Update AlphaCharacter.hx

--- source/AlphaCharacter.hx
+++ source/AlphaCharacter.hx
@@ -8,14 +8,10 @@ import Alphabet;
 class AlphaCharacter extends FlxText {
     public var row:Int = 0;
 
-    public function new(x:Float, y:Float, letter:String, textSize:Float, isBold:Bool) {
+    public function new(x:Float, y:Float, letter:String, textSize:Float, ?TextFont:String = "chaotix.ttf") {
         super(x, y, 0, letter, Std.int(42 * textSize));
         //42 братуха, 42
-        if (PlayState.SONG.song.toLowerCase() == 'found-you-legacy') { 
-		setFormat(Paths.font("sonic-cd-menu-font.ttf"), Std.int(42 * textSize), FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-        } else {
-		setFormat(Paths.font("chaotix.ttf"), Std.int(42 * textSize), FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-	}
+	setFormat(Paths.font(TextFont), Std.int(42 * textSize), FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         
         antialiasing = true;
     }
