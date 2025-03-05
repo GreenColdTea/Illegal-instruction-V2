3433859270e395017ba60302fd84a4513d1f1c6e
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-05 20:40:30 +0500

    Update MobileScaleMode.hx

--- source/mobile/MobileScaleMode.hx
+++ source/mobile/MobileScaleMode.hx
@@ -8,53 +8,55 @@ import flixel.system.scaleModes.BaseScaleMode;
  * @author: Karim Akra
  */
 
+import flixel.system.scaleModes.BaseScaleMode;
+
+/**
+ * ...
+ * @author: Karim Akra
+ */
 class MobileScaleMode extends BaseScaleMode
 {
-    public static var allowWideScreen(default, set):Bool = true;
+	public static var allowWideScreen(default, set):Bool = true;
 
-    override function updateGameSize(Width:Int, Height:Int):Void
-    {
-        if(ClientPrefs.noBordersScreen && allowWideScreen)
-        {
-            super.updateGameSize(Width, Height);
-        }
-        else
-        {
-            var ratio:Float = FlxG.width / FlxG.height;
-            var realRatio:Float = Width / Height;
-    
-            var scaleY:Bool = realRatio < ratio;
-    
-            if (scaleY)
-            {
-                gameSize.x = Width;
-                gameSize.y = Math.floor(gameSize.x / ratio);
-            }
-            else
-            {
-                gameSize.y = Height;
-                gameSize.x = Math.floor(gameSize.y * ratio);
-            }
-        }
-    }
+	override function updateGameSize(Width:Int, Height:Int):Void
+	{
+		if (ClientPrefs.wideScreen && allowWideScreen)
+		{
+			super.updateGameSize(Width, Height);
+		}
+		else
+		{
+			var ratio:Float = FlxG.width / FlxG.height;
+			var realRatio:Float = Width / Height;
 
-    override function updateGamePosition():Void
-    {
-        if(ClientPrefs.noBordersScreen && allowWideScreen)
-		    FlxG.game.x = FlxG.game.y = 0;
-        else
-            super.updateGamePosition();
-    }
+			var scaleY:Bool = realRatio < ratio;
 
-    @:noCompletion
-    private static function set_allowWideScreen(value:Bool):Bool
-    {
-        allowWideScreen = value;
-        FlxG.scaleMode = new MobileScaleMode();
-        return value;
-    }
+			if (scaleY)
+			{
+				gameSize.x = Width;
+				gameSize.y = Math.floor(gameSize.x / ratio);
+			}
+			else
+			{
+				gameSize.y = Height;
+				gameSize.x = Math.floor(gameSize.y * ratio);
+			}
+		}
+	}
 
-    public function new() {
-        super();
-    }
+	override function updateGamePosition():Void
+	{
+		if (ClientPrefs.wideScreen && allowWideScreen)
+			FlxG.game.x = FlxG.game.y = 0;
+		else
+			super.updateGamePosition();
+	}
+
+	@:noCompletion
+	private static function set_allowWideScreen(value:Bool):Bool
+	{
+		allowWideScreen = value;
+		FlxG.scaleMode = new MobileScaleMode();
+		return value;
+	}
 }
