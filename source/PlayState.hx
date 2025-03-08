cab58d2cfaa4b3aaccf3649d16863ddcc41533f9
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-09 02:32:16 +0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -132,11 +132,13 @@ class PlayState extends MusicBeatState
 	var jigglyOiledUpBlackMen:WiggleEffect;
 	var glitchinTime:Bool = false;
 
-   // IN THE SETUPMODCHART FUNCTION
+        // IN THE SETUPMODCHART FUNCTION
 	public var useModchart:Bool = true;
 
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
+
+	public var center:FlxPoint;
 
 	public static var ratingStuff:Array<Dynamic> = [
 		['You Suck!', 0.2], //From 0% to 19%
