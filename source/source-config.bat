91e50b7d10308c4076e51952150a8dfdab2e8513
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-09 00:13:35 +0500

    Update source-config.bat

--- source/source-config.bat
+++ source/source-config.bat
@@ -1,8 +1,10 @@
 @echo off
 cls
-title Illegal Instructions Necessary Libraries Installer for Compiling
+title Illegal Instruction Necessary Libraries Installer
 echo.
-haxelib setup C:
+echo Installing necessary libraries. Please wait...
+echo.
+haxelib setup C:\haxelib
 haxelib git openfl https://github.com/MobilePorting/openfl 9.3.4 > /dev/null --quiet
 haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --never --quiet
 haxelib git lime https://github.com/MobilePorting/lime.git > /dev/null --quiet
@@ -18,6 +20,13 @@ haxelib git hxvlc https://github.com/GreenColdTea/hxvlc --quiet --skip-dependenc
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc --quiet
 haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit.git --quiet
 haxelib list
+echo.
+echo Setting current library versions...
+echo.
+haxelib set flixel-tools 1.5.1
+haxelib set flixel-ui 2.6.3
+haxelib set flixel-addons 3.2.3
+haxelib set hscript 2.4.0
 echo.
 echo Done! Press any key to close the app!
 pause
