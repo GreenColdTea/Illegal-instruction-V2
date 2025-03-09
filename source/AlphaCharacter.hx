6913e7440ad41765439b01a4c3e2b3731788306e
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-09 19:50:53 +0500

    Update AlphaCharacter.hx

--- source/AlphaCharacter.hx
+++ source/AlphaCharacter.hx
@@ -9,7 +9,7 @@ class AlphaCharacter extends FlxText {
     public var row:Int = 0;
 
     public function new(x:Float, y:Float, letter:String, textSize:Float, isBold:Bool, ?textFont:String = "chaotix.ttf") {
-        super(x, y, 0, letter, Std.int(42 * textSize));
+        super(x, y, 0, letter, Std.int(42 * textSize), isBold, textFont);
         //42 братуха, 42
 	setFormat(Paths.font(textFont), Std.int(42 * textSize), FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         
