eb0e017fc55e56ddd7032a69c88140542624c853
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-12 15:26:27 +0500

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2078,7 +2078,7 @@ class FunkinLua {
 
 		//modchart shit
 		Lua_helper.add_callback(lua, "addMod", function(name:String, type:String) {
-                    var modManager = PlayState.instance.modManager;
+                    var modManager = PlayState.instance?.modManager;
 
                     if (modManager.get(name) != null) {
                         luaTrace('addMod: Modifier "' + name + '" already exists!', true);
