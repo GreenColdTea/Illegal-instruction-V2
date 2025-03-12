bd523ceb30093ec5f0077c1a74b77a1018a47fd4
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-13 04:16:38 +0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3371,7 +3371,6 @@ class PlayState extends MusicBeatState
 			}
 
                         modManager.receptors = [playerStrums.members, opponentStrums.members];
-			modManager.canMorchartOnLua = true;
 			callOnLuas('onPreModifierRegister', []);
 		   	modManager.registerDefaultModifiers();
 			callOnLuas('onPostModifierRegister', []);
