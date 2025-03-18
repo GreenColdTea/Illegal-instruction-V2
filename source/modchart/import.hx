43c09b5c7d0a22280a4b1fc6fda9d9ef05464520
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-18 19:16:09 +0500

    Update import.hx

--- source/modchart/import.hx
+++ source/modchart/import.hx
@@ -2,4 +2,5 @@ import modchart.Event.SetEvent;
 import modchart.Event.Event;
 import modchart.Event.EaseEvent;
 import modchart.Event.ModEvent;
+import modchart.Event.BaseEvent;
 import modchart.Event.FuncEvent;
