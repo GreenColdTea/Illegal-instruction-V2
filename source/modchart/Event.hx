a153ee9220d2c014ccba0aa31b34dfe8ea3fd809
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-18 19:12:54 +0500

    Update Event.hx

--- source/modchart/Event.hx
+++ source/modchart/Event.hx
@@ -15,6 +15,20 @@ class Event {
   public function run(curStep:Float){}
 }
 
+class BaseEvent {
+    public var manager:ModManager;
+    public var executionStep:Float = 0;
+	public var ignoreExecution:Bool = false;
+    public var finished:Bool = false;
+	public function new(step:Float, manager:ModManager)
+	{
+		this.manager = manager;
+		this.executionStep = step;
+	}
+
+    public function run(curStep:Float){}
+}
+
 class FuncEvent extends Event {
   public var callback:Void->Void;
 
