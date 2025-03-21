576c4cc9a092ba10f838d07cecfd621155dfa11a
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-21 23:52:13 +0500

    Delete source/modchart directory

--- source/modchart/import.hx
+++
@@ -1,8 +0,0 @@
-import modchart.BaseEvent.SetEvent;
-import modchart.BaseEvent.BaseEvent as Event;
-import modchart.BaseEvent.EaseEvent;
-import modchart.BaseEvent.ModEvent;
-import modchart.BaseEvent.BaseEvent;
-import modchart.BaseEvent.FuncEvent;
-import modchart.BaseEvent.CallbackEvent;
-import modchart.BaseEvent.StepCallbackEvent;
