7ff1354bd82c6d22a39c946e397461d328a3fd29
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-22 03:00:52 +0500

    Update ModCharts.hx

--- source/ModCharts.hx
+++ source/ModCharts.hx
@@ -4,6 +4,14 @@ import modchart.*;
 import flixel.math.FlxAngle;
 
 class ModCharts {
+    static function numericForInterval(start, end, interval, func){
+        var index = start;
+        while(index < end){
+            func(index);
+            index += interval;
+        }
+    }
+    
     public static function lesGo(shit:PlayState, modManager:ModManager, songName:String) {
         switch (songName) {
                 case "breakout":
