8c1b311122ad35018e6e12f478c95526409a46b8
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-29 15:14:20 +0500

    Update HaxelibInstaller.hx

--- setup/HaxelibInstaller.hx
+++ setup/HaxelibInstaller.hx
@@ -1,7 +1,16 @@
 class HaxelibInstaller {
     static function main() {
-        var platform = Sys.systemName().toLowerCase();
-        var path = "setup/" + (platform == "windows" ? "windows" : "android") + "/haxelib.json";
-        Sys.command("haxelib install " + path + " --quiet");
+        var sysName = Sys.systemName().toLowerCase();
+        var path = switch sysName {
+            case "windows": "setup/windows/haxelib";
+            case "linux", "mac": "setup/android/haxelib";
+            default: null;
+        }
+
+        if (path != null) {
+            Sys.command("haxelib", ["install", path, "--quiet"]);
+        } else {
+            trace("Unsupported OS: " + sysName);
+        }
     }
 }
