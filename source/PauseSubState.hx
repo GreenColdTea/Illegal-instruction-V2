9546f027822a07346027bddade47ddbf9ec61ea7
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2025-03-31 12:56:54 +0500

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -78,7 +78,7 @@ class PauseSubState extends MusicBeatSubstate
 		}
 		menuItems = menuItemsOG;
 
-	        startY = (FlxG.height / 2) - (spacing * 1.5) + 50;
+	        startY = (FlxG.height / 2) - (spacing * 1.5) + 75;
 	
 	        curRender = PlayState.instance.dad.curCharacter;
 
